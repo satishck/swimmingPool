52fd38adfeb1c3ab4b42d987161f80e7de8d572a
satish <satish312@gmail.com>
2023-02-27 16:10:49 +0530

    app9

---
+++ app9/src/H.java
@@ -0,0 +1,16 @@
+
+class H
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("main begin");
+		if (true)
+	        if (true)
+		   System.out.println("if2-begin");
+			
+	     
+	    
+	System.out.println("main end");
+}
+}
+
---
+++ app9/src/M.java
@@ -0,0 +1,15 @@
+class M
+{
+public static void main(String[] args) 
+{
+System.out.println("main begin");
+if (false)
+if (true)
+if (true)
+System.out.println("if3-begin");
+						
+					
+				
+	System.out.println("main end");
+}
+}
